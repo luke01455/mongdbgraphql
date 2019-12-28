92e196f7f78c451d9695d591c4466de49d394a1b
luke01455 <luke0145555@hotmail.co.uk>
2019-12-28 18:23:08 +0000

    completed register with validation

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,13 +1,39 @@
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
-const User = require('../../models/Users')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { UserInputError } = require('apollo-server')
 
+const { validateRegisterInput, validateLoginInput } = require('../../utils/validators')
 const { SECRET_KEY } = require('../../config/config')
+const User = require('../../models/User')
+
+
 
 module.exports = {
     Mutation: {
         async register(parent, { registerInput : { username, email, password, confirmPassword }}, context, info) {
-            // TODO: validate user data
+            const  { valid, errors } = validateRegisterInput(username, email, password, confirmPassword)
+            if(!valid) {
+                // pass the errors from the validate file
+                throw new UserInputError('Errors', { errors })
+            }
+            const userExists = await User.findOne({ username })
+            const emailExists = await User.findOne({ email })
+
+            if (userExists) {
+                throw new UserInputError('Username already exists', {
+                    error: {
+                        username: 'This username is taken'
+                    }
+                })
+            }
+            
+            if (emailExists) {
+                throw new UserInputError('Email already exists', {
+                    error: {
+                        email: 'This email is taken'
+                    }
+                })
+            }
             password = await bcrypt.hash(password, 12);
 
             const newUser = new User({
